2e860b3f12bb68c24524c68d5ae7beb816bb14f7
Yann Deo <yan2sambou@gmail.com>
2020-03-03 10:40:02 +0100

    configuration de la base de donnée local et prod

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -21,6 +21,11 @@ class Product
      */
     private $name;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $quantity;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -34,6 +39,18 @@ class Product
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?string
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(string $quantity): self
+    {
+        $this->quantity = $quantity;
 
         return $this;
     }
