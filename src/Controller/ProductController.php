ce01f253bef8d862d31c72277796b9a1191240f3
Yann Deo <yan2sambou@gmail.com>
2020-03-21 19:23:13 +0100

    test router /test

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/", name="product")
+     * @Route("/test", name="product")
      */
     public function index()
     {
