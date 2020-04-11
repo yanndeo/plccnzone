6f1149d938ac0b2da8be5477f37176c61a098d9f
Yann Deo <yan2sambou@gmail.com>
2020-04-11 16:19:20 +0100

    maj file capistranox

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,10 +46,10 @@ namespace :deploy do
     after :updated, :php_sf do
         invoke "php:composer"
         invoke "symfony:migrate"
-        invoke "symfony:optimize"
         invoke "webapp:install"
         invoke "webapp:optimize"
         invoke "webapp:manifest"
+        invoke "symfony:optimize"
     end
 
     after :finished, 'php:restart_fpm'
